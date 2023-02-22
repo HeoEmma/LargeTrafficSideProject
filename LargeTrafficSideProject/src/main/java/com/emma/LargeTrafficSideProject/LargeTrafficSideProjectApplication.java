abcf452b1557cface7d5339c14086d4530e31a87
Emma Heo <eunseong8@gmail.com>
2023-02-23 01:44:11 +0900

    initialize with spring initializr A side project for large traffic handling training

---
+++ LargeTrafficSideProject/src/main/java/com/emma/LargeTrafficSideProject/LargeTrafficSideProjectApplication.java
@@ -0,0 +1,13 @@
+package com.emma.LargeTrafficSideProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeTrafficSideProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeTrafficSideProjectApplication.class, args);
+	}
+
+}
---
+++ LargeTrafficSideProject/src/test/java/com/emma/LargeTrafficSideProject/LargeTrafficSideProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emma.LargeTrafficSideProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeTrafficSideProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
